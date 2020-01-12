ea997e542f94b08aad468ccd2ee1618a015485d0
Jacob <jrammer101@gmail.com>
2020-01-11 19:03:49 -0800

    completed compute fib, error handling, and printing

---
+++ Lab0/jrammer_lab0.py
@@ -0,0 +1,78 @@
+"""
+Jacob Rammer
+Lab 0
+"""
+
+
+class FibSequence:
+    """
+    Calculate Fibonacci Numbers
+    """
+
+    def __init__(self):
+        self.array = [0, 1]
+        self.fib_num = 0
+        self.ord_indicator = ""
+
+    def calculate(self, n: int):
+        """
+        Method to calculate the fibonacci sequence by appending
+        to a list.
+        :param n: what is the nth fibonacci number
+        :return: void
+        """
+
+        for i in range(n):
+            self.array.append(self.array[i] + self.array[i + 1])
+        self.fib_num = self.array[n - 1]
+        self.display_message(n)
+
+    def display_message(self, n: int):
+        """
+        Dynamically updates the ordinal indicator to the proper
+        format. 1st, 2nd, 3rd etc
+        :param n: nth Fibonacci number
+        :return: void
+        """
+
+        temp_val = str(n)  # cast n to str to keep ifs cleaner
+
+        if 10 < int(temp_val) < 20:  # teens are a special case
+            self.ord_indicator = "th"
+        elif temp_val[::-1][0] == "1":
+            self.ord_indicator = "st"
+        elif temp_val[::-1][0] == "2":
+            self.ord_indicator = "nd"
+        elif temp_val[::-1][0] == "3":
+            self.ord_indicator = "rd"
+        else:
+            self.ord_indicator = "th"
+
+
+def main():
+    """
+    Main function bla bla yada yada
+    :return: void
+    """
+
+    while True:
+        try:  # try to get valid input
+            nth_fib = input("Enter a digit or \'q\' to quit: ").lower()
+            if len(nth_fib) == 0:
+                print("Please enter a positive integer or \'q\'\n")
+                main()
+            if nth_fib == "q":
+                exit()
+            nth_fib = int(nth_fib)
+            if int(nth_fib) > 0:
+                fib_num = FibSequence()
+                fib_num.calculate(int(nth_fib))
+                print(f"The {nth_fib}{fib_num.ord_indicator} Fibonacci number is: {fib_num.fib_num}\n")
+            else:
+                print("Please enter a positive integer or \'q\'")
+        except ValueError:  # if a single char is entered, don't crash
+            print("Please enter a positive integer or \'q\'\n")
+
+
+if __name__ == "__main__":
+    main()
