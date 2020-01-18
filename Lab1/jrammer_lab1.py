a3f188013c53393b54549dae4e883917e8bd273c
Jacob <jrammer101@gmail.com>
2020-01-17 17:07:38 -0800

    Finished stack class

--- Lab1/jrammer_lab1.py
+++ Lab1/jrammer_lab1.py
@@ -74,12 +74,83 @@ class Queue:
         return ticket
 
 
+class Stack:
+    """
+    Stacks are first in last out
+    """
+
+    def __init__(self, size: int):
+        self.head = 0
+        self.max_size = size
+        self.current_size = 0
+        self.data = []  # need to change to linked list
+
+    def is_empty(self) -> bool:
+        """
+        Checks to see if the stack is empty
+        :return: True is empty, otherwise false
+        """
+
+        return self.current_size == 0
+
+    def is_full(self) -> bool:
+        """
+        Checks to see if the stack is full
+        :return: true if full, otherwise false
+        """
+
+        return self.current_size == self.max_size
+
+    def push(self, ticket: str) -> bool:
+        """
+        If the stack is empty, append the newest data to top of the stack
+        :return: True if successful, otherwise false
+        """
+
+        if self.is_full():
+            return False
+        self.data.append(ticket)
+        self.current_size += 1
+        self.head += 1
+
+    def pop(self) -> MealTicket or False:
+        """
+        Get the last datatype from the top of the stack.
+        :return: Mealticket if stack has data, otherwise false
+        """
+
+        if self.is_empty():
+            return False
+        self.head -= 1
+        self.current_size -= 1
+        return self.data[self.head]
+
+    def peek(self) -> MealTicket or False:
+        """
+        Peek at the first element of the stack without deleting the item
+        :return: Mealticket is stack has data, otherwise false
+        """
+
+        if self.is_empty():
+            return False
+        return self.data[0]
+
+
 def main():
     test = Queue(2)
-    print(test.is_empty())
-    print(test.is_full())
-    print(test.enqueue("test"))
-    print(test.dequeue())
+    # print(test.is_empty())
+    # print(test.is_full())
+    # print(test.enqueue("test"))
+    # print(test.dequeue())
+
+    stack = Stack(2)
+    # stack.current_size = 2
+    # print(stack.is_empty())
+    # print(stack.is_full())
+    print(stack.push("test"))
+    # print(stack.pop())
+    # print(stack.pop())
+    print(stack.peek())
 
 
 main()
