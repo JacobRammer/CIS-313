e752ee7f8719379d174d4abfdc5584d66331b99b
jacobrammer <jrammer101@gmail.com>
2020-02-12 15:36:31 -0800

    i know what teh bug is

--- Lab2/jrammer_lab2.py
+++ Lab2/jrammer_lab2.py
@@ -28,7 +28,7 @@ class PriorityQueue:
 
         return self._length == self._maxSize
 
-    def swap(self, index, ticket):
+    def swap(self, index):
         """
         Swap the child and parent node if required to keep proper
         tree properties
@@ -37,12 +37,36 @@ class PriorityQueue:
         :return: void
         """
 
-        self._heap[index] = ticket
-        while index > 1 and \
-                self._heap[self.getParent(index)] < self._heap[index]:
-            self._heap[index], self._heap[self.getParent(index)] = \
-                self._heap[self.getParent(index)], self._heap[index]
-            index = self.getParent(index)
+        # self._heap[index] = ticket
+        # parent_index = self.getParent(index)
+        # while index > 1 and self._heap[parent_index] < self._heap[index]:
+        #     self._heap[index], self._heap[parent_index] = \
+        #         self._heap[parent_index], self._heap[index]
+        #     print(f"index is {index}")
+        #     parent_index = self.getParent(index)
+        #     index = self.getParent(index)
+        # print(f"index 1 is {index}")
+
+        parent = self.getParent(index)
+        left_child = self.getLeftChild(parent)
+        right_child = self.getRightChild(parent)
+
+        if parent == 0:
+            return
+
+        if left_child <= parent and self._heap[left_child] \
+                < self._heap[index]:
+            largest = left_child
+        else:
+            largest = index
+        if right_child <= parent and self._heap[right_child] \
+                < self._heap[largest]:
+            largest = right_child
+
+        if largest != index:
+            self._heap[index], self._heap[largest] = \
+                self._heap[largest], self._heap[index]
+            self.swap(largest)
 
     def insert(self, ticket):
         """
@@ -55,8 +79,8 @@ class PriorityQueue:
         if self.isFull():
             return False
         self._length += 1
-        self._heap[self._length] = -1  # a mealticket's total will never be < 0
-        self.swap(self._length, ticket)
+        self._heap[self._length] = ticket
+        self.swap(self._length)
 
     def getParent(self, index):
         """
@@ -67,14 +91,36 @@ class PriorityQueue:
 
         return index // 2
 
+    def getLeftChild(self, index):
+        """
+        Get the index of the left child
+        :param index: index of parent
+        :return: int
+        """
+
+        return 2 * index
+
+    def getRightChild(self, index):
+        """
+        Get the index of the right child
+        :param index: index of parent
+        :return: int
+        """
+
+        return (2 * index) + 1
+
+
 def main():
     pq = PriorityQueue(7)
     pq.insert(5)
-    pq.insert(4)
-    pq.insert(6)
     pq.insert(9)
-    print(pq._length)
-    pq.insert(8)
+    # pq.insert(4)
+    # pq.insert(6)
+    # pq.insert(8)
+
+    # print(pq._length)
+    # pq.insert(8)
     print(pq._heap)
+
 
 main()
